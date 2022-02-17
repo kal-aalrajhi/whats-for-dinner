0339863c866c4a64707a175626dc2e9d597541b4
Kal <kal.aalrajhi@gmail.com>
2022-02-16 22:32:50 -0700

    Create arrays of foods and randomly select an item from chosen array

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 // Global variables
 var letsCookButton = document.querySelector('#cook-button');
-
+var foodChoices = document.getElementsByName('food-choice');
+var selectedChoice;
 // Event listeners
 letsCookButton.addEventListener('click', submitChoice);
 
 // Functions
 function submitChoice() {
-    // something happens
+    for (var i = 0; i < foodChoices.length; i++) {
+        if(foodChoices[i].checked) {
+            selectedChoice = foodChoices[i].value;
+        }
+    }
+}
+
+function generateRandomFoodItem(foodType) {
+    if (foodType === "sides") {
+        return sides[randomIndex(sides)];
+    } else if (foodType === "dishes") {
+        return mains[randomIndex(dishes)];
+    } else if (foodType === "desserts") {
+        return desserts[randomIndex(desserts)];
+    } else {
+        return "Invalid food type";
+    }
+}
+
+function generateRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
 }
