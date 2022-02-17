e3ab1888cb4eb0e5661274e9953c32767ff84255
Kal <kal.aalrajhi@gmail.com>
2022-02-16 17:51:46 -0700

    Outline button click event and color change

---
+++ main.js
@@ -0,0 +1,10 @@
+// Global variables
+var letsCookButton = document.querySelector('#cook-button');
+
+// Event listeners
+letsCookButton.addEventListener('click', submitChoice);
+
+// Functions
+function submitChoice() {
+    // something happens
+}
